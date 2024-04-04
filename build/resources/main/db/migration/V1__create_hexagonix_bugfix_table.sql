848798677a4dbd27fb4903bf7b31c80a867614b4
Felipe Lunkes <felipe.lunkes@orsegups.com.br>
2024-04-04 02:45:58 -0300

    First commit

--- build/resources/main/db/migration/V1__create_hexagonix_bugfix_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE hexagonix_bugfix (
-fix_id VARCHAR(50) PRIMARY KEY,
-description VARCHAR(1000) NOT NULL,
-resolution VARCHAR(1000) NOT NULL,
-committed BOOLEAN DEFAULT FALSE,
-release_date TIMESTAMP(6) NOT NULL,
-version_id VARCHAR(50)
-);
--- build/resources/main/db/migration/V3__create_version_has_fix_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE version_has_fix (
-version_id VARCHAR(50) NOT NULL,
-fix_id VARCHAR(50) NOT NULL
-);
