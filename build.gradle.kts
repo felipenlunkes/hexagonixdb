5ff90613afc5714eab781d752385eeb6d6cc543a
Felipe Lunkes <felipe.lunkes@orsegups.com.br>
2024-04-03 23:02:53 -0300

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+	id ("org.flywaydb.flyway") version "8.2.0"
+}
+
+flyway {
+	schemas = arrayOf("hexagonixdb")
+	user = project.properties["databaseUser"].toString()
+	url = project.properties["databaseUrl"].toString()
+	password = project.properties["databasePassword"].toString()
+}
+
+group = "br.lunx"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-mysql")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:mysql")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/br/lunx/hexagonixdb/hexagonixDB/HexagonixDbApplication.java
@@ -0,0 +1,13 @@
+package br.lunx.hexagonixdb.hexagonixDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonixDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonixDbApplication.class, args);
+	}
+
+}
